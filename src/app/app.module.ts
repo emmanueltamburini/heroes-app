d0371af925c5a8d3b0adb333409c3e974770c8ab
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-09-12 19:22:27 -0400

    Router has been configurated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
@@ -12,7 +15,8 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
