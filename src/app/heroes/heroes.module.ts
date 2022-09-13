7bca4863839d2ff8552de6a31c13aa8bf7b10d4f
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-09-13 19:51:08 -0400

    first home view version has been added

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AddComponent } from './pages/add/add.component';
-import { SearchComponent } from './pages/search/search.component';
-import { HeroByIdComponent } from './pages/hero-by-id/hero-by-id.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AllHeroesComponent } from './pages/all-heroes/all-heroes.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
+
+import { AddComponent } from './pages/add/add.component';
+import { AllHeroesComponent } from './pages/all-heroes/all-heroes.component';
+import { HeroByIdComponent } from './pages/hero-by-id/hero-by-id.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
 
 
 @NgModule({
@@ -21,6 +23,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   imports: [
     CommonModule,
     FlexLayoutModule,
+    MaterialModule,
     HeroesRoutingModule
   ]
 })
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   exports: [
-
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSidenavModule,
   ]
 })
 export class MaterialModule { }
